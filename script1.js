e3d2e204cfaa261408f1826b6e6d62cfe774f5ea
chandanpratap <36849958+chandanpratap@users.noreply.github.com>
2018-03-01 18:35:16 +0530

    Book Search Files

---
+++ script1.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function(){
+	
+	$("#form1").submit(function(){
+		 var search=$("#books").val();
+		 if(search =='')
+		 {
+			 alert("please enter something in the field first");
+		 }
+		  else{
+			  var url ='';
+			  var img ='';
+			  var title='';
+			  var author ='';
+			  
+			  $.get("https://www.googleapis.com/books/v1/volumes?q"+search,function(response){
+				  
+				  
+				  //console.log(response);
+				  
+				  
+				  for(i=0;i<response.items.length;i++)
+				  {
+					title=$('<h5 class="center-align black-text">' + response.items[i].volumeInfo.title + '</h5>');
+					author=$('<h5 class="center-align black-text"> By:' + response.items[i].volumeInfo.authors + '</h5>');
+					img=$('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning"> Read More</button> + </a>');
+						url=response.items[i].volumeInfo.imageLinks.thumbnail;
+                       img.attr('src',url);
+
+                        title.appendTo("#result");
+                         author.appendTo("#result");
+                         img.appendTo("#result");						
+				  }
+				  
+			  });
+			  
+		  }
+		
+		
+	});
+	
+	return false;
+	
+});
